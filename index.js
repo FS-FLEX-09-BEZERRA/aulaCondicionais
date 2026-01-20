4c5aa2e87634814599ce6289083d67544c473d2c
nayaracalenzo <queiroz.14@hotmail.com>
2026-01-20 20:38:38 -0300

    introdução if else

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+//Condicionais
+
+// if else
+
+let tomeiCafe = prompt("Bom dia, tomou café? Responda sim ou não")
+
+if (tomeiCafe === 'sim') {
+  console.log("Bom dia pra você também")
+} else {
+  console.log("Bom dia pra quem?")
+}
+
+// switch case
